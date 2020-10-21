93ac9f3df28357f8d43aaa9508bd4b2a66ff3408
Manuela Redinciuc <manuela.redinciuc@Nology.local>
2020-10-21 17:59:36 +0100

    fixed risponsivness

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../sass/main.module.scss";
+import styles from "./LandingPage.module.scss";
 import imageStepOne from '../../Assets/images/image1.png';
 import imageStepTwo from '../../Assets/images/image2.png';
 import imageStepThree from '../../Assets/images/image3.png';
@@ -7,7 +7,7 @@ import imageStepThree from '../../Assets/images/image3.png';
 const LandingPage = () => {
     return(
         <section className={styles.landingPage}>
-            <div className={styles.grid}>
+            <div className={styles.gridContainer}>
                 <div className={styles.topLeft}>
                     <div className={styles.circle}></div>
                 </div>
@@ -18,7 +18,12 @@ const LandingPage = () => {
                 <div className={styles.topRight}>
                     <div className={styles.semiCircle}></div>
                 </div>
-                <div className={styles.howToo}>
+                <div className={styles.bottomRight}>
+                    <button className={styles.LandingPageButton}>Sign In ></button>
+                    <button className={styles.LandingPageButton}>View Offers ></button>
+                </div>
+            </div>
+            <div className={styles.registration}>
                     <div className={styles.registrationProcess}>
                         <img src={imageStepOne} alt="icon"></img>
                         <p> 1) Discover the finest nearby restaurants, cafes and bars, get exclusive discounts and limited time offers.</p>
@@ -31,11 +36,6 @@ const LandingPage = () => {
                         <img src={imageStepThree} alt="icon"></img>
                         <p> 3) Present your unique code to the venue, then sit back and enjoy your discounted meal!</p>
                     </div>
-                </div>
-                <div className={styles.bottomRight}>
-                    <button className={styles.LandingPageButton}>Sign In ></button>
-                    <button className={styles.LandingPageButton}>View Offers ></button>
-                </div>
             </div>
         </section>
     )
