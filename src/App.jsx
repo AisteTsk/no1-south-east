3323006f764d1a4a2c65171ca8385713d6efed6b
Ollie Robins <ollierobins93@gmail.com>
2020-10-26 17:29:23 +0000

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import styles from './App.module.scss';
 import Routes from "./containers/Routes"
-import restaurants from "./data/restaurants";
 
 const App = () => {
 
