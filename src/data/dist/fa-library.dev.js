fdb65850ce590716ee7a12c465e55087c6754b84
Danny Lambert <dlambert150293@gmail.com>
2020-10-26 14:21:23 +0000

    .

---
+++ src/data/dist/fa-library.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _fontawesomeSvgCore = require("@fortawesome/fontawesome-svg-core");
+
+var _freeSolidSvgIcons = require("@fortawesome/free-solid-svg-icons");
+
+// import { faHeart as faOpenHeart } from "@fortawesome/free-regular-svg-icons";
+// import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+var _default = _fontawesomeSvgCore.library.add(_freeSolidSvgIcons.faUtensils, _freeSolidSvgIcons.faGlassCheers, _freeSolidSvgIcons.faFilter, _freeSolidSvgIcons.faSearch);
+
+exports["default"] = _default;
