5620076b61b700e289ee1c6e057ba0d7c7ac342a
Manuela Redinciuc <manuela.redinciuc@Nology.local>
2020-10-28 14:30:43 +0000

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import './App.module.css';
-import Restaurant from '../src/data/restaurants';
-import LandingPage from "./components/LandingPage/LandingPage";
 import styles from "./App.module.scss";
 import library from "./data/fa-library";
 import Routes from "./containers/Routes"
