d68731a8dcac24b18ab288781822e09f7b793612
Manuela Redinciuc <manuela.redinciuc@Nology.local>
2020-10-28 16:10:15 +0000

    changed based on Ellis's notes

--- src/data/dist/fa-library.dev.js
+++ src/data/dist/fa-library.dev.js
@@ -7,19 +7,8 @@ exports["default"] = void 0;
 
 var _fontawesomeSvgCore = require("@fortawesome/fontawesome-svg-core");
 
-<<<<<<< HEAD
 var _freeSolidSvgIcons = require("@fortawesome/free-solid-svg-icons");
 
-// import { faHeart as faOpenHeart } from "@fortawesome/free-regular-svg-icons";
-// import { faYoutube } from "@fortawesome/free-brands-svg-icons";
-=======
-var _freeRegularSvgIcons = require("@fortawesome/free-regular-svg-icons");
-
-var _freeBrandsSvgIcons = require("@fortawesome/free-brands-svg-icons");
-
-var _freeSolidSvgIcons = require("@fortawesome/free-solid-svg-icons");
-
->>>>>>> dd144b54cd4b9821562a007fac806739a4a98f34
 var _default = _fontawesomeSvgCore.library.add(_freeSolidSvgIcons.faUtensils, _freeSolidSvgIcons.faGlassCheers, _freeSolidSvgIcons.faFilter, _freeSolidSvgIcons.faSearch);
 
 exports["default"] = _default;
