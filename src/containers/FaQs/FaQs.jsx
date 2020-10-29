557c51ae54eee7f7b7ae003bc6630eb5634ca47c
Ellis Hornabrook <ellishornabrook@gmail.com>
2020-10-29 17:36:26 +0000

    T&Cs, FAQ's, Logo component complete with links

---
+++ src/containers/FaQs/FaQs.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./FaQs.module.scss";
+
+const FaQs = () => {
+
+  return (
+    <section className={styles.background}>
+      <div className={styles.feedback}>
+        <h2>FAQ's</h2>
+        <div>
+            <p>How do I sign up?</p>
+            <p>Head to this link and register for your first 2 months free!</p>
+        </div>
+        <div>
+            <p>How much is membership?</p>
+            <p>Only £4.99 per month</p>
+        </div>
+        <div>
+            <p>Where are your offers located?</p>
+            <p>Currently we have offerings across the South East of London in areas such as Beckenham, Bromley, Greenwich, Orpington, Petts Wood, Locksbottom and Sevenoaks. New restaurants are continually being added to keep your eyes peeled for new additions.</p>
+        </div>
+        <div>
+            <p>Will you be expanding to other locations?</p>
+            <p>Yes, work is currently underway to launch No.1 in other areas</p>
+        </div>
+        <div>
+            <p>Can I cancel my membership?</p>
+            <p>Yes, you can cancel your membership at any point by……</p>
+        </div>
+        <div>
+            <p>I went to a restaurant but they refused to accept my offer, what do I do?</p>
+            <p>Thanks for getting in touch…..please contact us by email here so we can investigate this.</p>
+        </div>
+        <div>
+            <p>How do I contact you?</p>
+            <p>You can email us at info@no1southeast.co.uk</p>
+        </div>
+        <div>
+            <p>What times and days can I use the offers?</p>
+            <p>Each offer is individual and the finer details are outlined within each offering</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FaQs;
