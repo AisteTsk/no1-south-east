5442c7e15670a69f8d240b21185be56d5429bd2f
Danny Lambert <dlambert150293@gmail.com>
2020-10-29 16:51:34 +0000

    mock user data input again

---
+++ src/data/mockUserData.js
@@ -0,0 +1,101 @@
+const mockUserData = [
+    {
+        'firstName': 'Danny',
+        'secondName': 'Lambert',
+        'email': 'danny@testest.com',
+        'password': 'password',
+        'phoneNumber': '07788996622',
+        'postcode': 'SW2 4ER',
+    },
+
+    {
+        'firstName': 'Manuela',
+        'secondName': 'Redinicuic',
+        'email': 'manuela@testest.com',
+        'password': 'password',
+        'phoneNumber': '07799996622',
+        'postcode': 'SW2 4EL',
+    },
+
+    {
+        'firstName': 'Harry',
+        'secondName': 'Holden',
+        'email': 'harry@testest.com',
+        'password': 'password',
+        'phoneNumber': '07709996622',
+        'postcode': 'SW2 4EW',
+    },
+
+    {
+        'firstName': 'Ellis',
+        'secondName': 'Hornabrook',
+        'email': 'ellis@testest.com',
+        'password': 'password',
+        'phoneNumber': '07719996622',
+        'postcode': 'SW2 4EE',
+    },
+
+    {
+        'firstName': 'Aiste',
+        'secondName': 'Tiskeviciute',
+        'email': 'aiste@testest.com',
+        'password': 'password',
+        'phoneNumber': '07729996622',
+        'postcode': 'SW2 4ET',
+    },
+
+    {
+        'firstName': 'Joe',
+        'secondName': 'Calleya',
+        'email': 'joe@testest.com',
+        'password': 'password',
+        'phoneNumber': '07729996622',
+        'postcode': 'SW2 4EY',
+    },
+
+    {
+        'firstName': 'Manuel',
+        'secondName': 'Lucas',
+        'email': 'manuel@testest.com',
+        'password': 'password',
+        'phoneNumber': '07739996622',
+        'postcode': 'SW2 4EY',
+    },
+
+    {
+        'firstName': 'Sophie',
+        'secondName': 'Roe',
+        'email': 'sophie@testest.com',
+        'password': 'password',
+        'phoneNumber': '07749996622',
+        'postcode': 'SW2 4EU',
+    },
+
+    {
+        'firstName': 'Ollie',
+        'secondName': 'Robbins',
+        'email': 'ollie@testest.com',
+        'password': 'password',
+        'phoneNumber': '07769996622',
+        'postcode': 'SW2 4EY',
+    },
+
+    {
+        'firstName': 'Ollie',
+        'secondName': 'Holden',
+        'email': 'holden@testest.com',
+        'password': 'password',
+        'phoneNumber': '07789996622',
+        'postcode': 'SE16 2NE',
+    },
+
+    {
+        'firstName': 'Bex',
+        'secondName': 'Fennelly',
+        'email': 'holden@testest.com',
+        'password': 'password',
+        'phoneNumber': '0770096622',
+        'postcode': 'SW2 4ED',
+    },
+]
+export default mockUserData;
