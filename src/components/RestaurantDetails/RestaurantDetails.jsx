c03bcfd9f57196dd1f349cd56870d378e1c0c338
Ollie Robins <ollierobins93@gmail.com>
2020-11-02 14:41:08 +0000

    changes made to styling

--- src/components/RestaurantDetails/RestaurantDetails.jsx
+++ src/components/RestaurantDetails/RestaurantDetails.jsx
@@ -31,7 +31,7 @@ const RestaurantDetails = (props) => {
     //this function will take the array of Booleans, filter for true and show
 
     const ConvertBooleanToText = (inputBooleanArray) => {
-        const outputString = Object.keys(inputBooleanArray).filter((x) => (inputBooleanArray[x])).toString();
+        const outputString = Object.keys(inputBooleanArray).filter((x) => (inputBooleanArray[x])).join(', ');
         return outputString ;
     };
 
@@ -48,13 +48,12 @@ const RestaurantDetails = (props) => {
             <p>{offerDescription}</p><br/>
             <p>Restaurant Info:</p>
             <p>{restaurantDescription}</p><br/>
-            <p>Cuisine: {cuisine}</p><br/>
+            <p>Cuisine: {cuisine.join(', ')}</p><br/>
             <p>Sitting: {ConvertBooleanToText(sitting)}</p><br/>
             <p>Valid until: {validUntil}</p><br/>
-            <p>Days Avalible: {daysAvailable.toString()}</p><br/>
+            <p>Days Avalible: {daysAvailable.join(', ')}</p><br/>
             <p>Maximim Table Size: {maximumTableSize}</p> <br/>           
             <p>{ConvertBooleanToText(dietaryRequirements)}</p>
-             
             <span className={styles.fontawesomeContainer}>
                 <a href={instagram} target="_blank" > 
                     <FontAwesomeIcon icon={["fab", "instagram"]}/>
