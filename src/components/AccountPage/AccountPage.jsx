25ac82128f80de59d7c4c983c28ce7e3af4d1a1c
Sophie Roe <sophie.roe@Nology.local>
2020-11-06 17:43:48 +0000

    updated styling

--- src/components/AccountPage/AccountPage.jsx
+++ src/components/AccountPage/AccountPage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from '@reach/router'
 import styles from './AccountPage.module.scss'
 import Logo from "../Logo/Logo";
 import favourites from '../../data/restaurants'
@@ -11,24 +10,22 @@ const AccountPage = (props) => {
 
     const { signOut } = props;
 
-        // we have used a placeholder for the FAVs, Saved and redeemed
+    // we have used a placeholder for the FAVs, Saved and redeemed
 
     const contentJsx = favourites.length ? (
-        <CardList restaurants={favourites}/>
+        <CardList restaurants={favourites} />
     ) : (
-        <FeedbackPanel
-            header="No matches"
-            text="None of our restaurants matched that search"
-        />
+            <FeedbackPanel
+                header="No matches"
+            />
         )
     return (
         <div>
             <a name="profile"></a>
             <div className={styles.page}>
-
                 <Logo />
                 <div className={styles.profileBox}>
-                <section className={styles.form}>
+                    <section className={styles.form}>
                         <h3>Update your info</h3>
                         <form action="">
                             <div className={styles.form_container}>
@@ -37,51 +34,49 @@ const AccountPage = (props) => {
                             </div>
                             <div className={styles.form_container}>
                                 <input type="text" name="email" placeholder="Email Address" required />
-
                                 <input type="text" name="password" placeholder="Password" required />
                                 <input type="text" name="confirmpassword" placeholder="Confirm Password" required />
                             </div>
                             <button type="submit" className={styles.account_btn}>Update</button>
                             <button onClick={signOut} className={styles.account_btn}>Sign out</button>
                         </form>
-                </section>
+                    </section>
                 </div>
 
+                <div className={styles.navButtons}>
+                    <div className={styles.buttons}>
+                        <a href="#profile">
+                            <button className={styles.navButton}>Profile</button>
+                        </a>
 
-                <div className={styles.bottomRight}>
-                            <div className={styles.buttons}>
-                            <a href="#profile">
-                                    <button className={styles.LandingPageButton}>Profile</button>
-                                </a>
+                        <a href="#favourites">
+                            <button className={styles.navButton}>Favourites</button>
+                        </a>
 
-                                <a href="#favourites">
-                                    <button className={styles.LandingPageButton}>Favourites</button>
-                                </a>
-
-                                <a href="#recentlyViewed">
-                                    <button className={styles.LandingPageButton}>Recently Viewed</button>
-                                </a>
-                                <a href="#redeemed">
-                                    <button className={styles.LandingPageButton}>Redeemed Offers</button>
-                                </a>
-                                <a href="#referFriend">
-                                    <button className={styles.LandingPageButton}> Refer a Friend</button>
-                                </a>
-                            </div>
-                        </div>
+                        <a href="#recentlyViewed">
+                            <button className={styles.navButton}>Recently Viewed</button>
+                        </a>
+                        <a href="#redeemed">
+                            <button className={styles.navButton}>Redeemed Offers</button>
+                        </a>
+                        <a href="#referFriend">
+                            <button className={styles.navButton}> Refer a Friend</button>
+                        </a>
+                    </div>
+                </div>
             </div>
 
             <div className={styles.page}>
-            <a name="favourites"></a>
-            <h2 className={styles.boxtitle}>Favourites</h2>
+                <a name="favourites"></a>
+                <h2 className={styles.boxtitle}>Favourites</h2>
                 <section className={styles.form}>
                     {contentJsx}
                 </section>
             </div>
 
             <div className={styles.page}>
-            <a name="recentlyViewed"></a>
-            <h2 className={styles.boxtitle}> Recently Viewed</h2>
+                <a name="recentlyViewed"></a>
+                <h2 className={styles.boxtitle}> Recently Viewed</h2>
                 <section className={styles.form}>
                     {contentJsx}
                 </section>
@@ -96,15 +91,14 @@ const AccountPage = (props) => {
             </div>
 
             <div className={styles.page}>
-            <a name="referFriend"></a>
-            <h2 className={styles.boxtitle}>Refer a Friend</h2>
+                <a name="referFriend"></a>
+                <h2 className={styles.boxtitle}>Refer a Friend</h2>
                 <section className={styles.form}>
-                <div className={styles.refer}>
-                    <button className={styles.account_btn}>Click for a referral code:</button>
-                   <p></p>
+                    <div className={styles.refer}>
+                        <button className={styles.account_btn}>Click for a referral code:</button>
                     </div>
                 </section>
-                
+
             </div>
         </div>
     )
