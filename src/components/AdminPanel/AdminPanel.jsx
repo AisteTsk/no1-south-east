bca30c7908e6e42aec3ad16671587fd31c4a89fd
Aiste Tisk <aiste.tisk@Nology.local>
2020-11-10 17:51:44 +0000

    bare-bone structure for admin panel

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -6,29 +6,28 @@ import 'semantic-ui-css/semantic.min.css'
 
 const AdminPanel = () => {
 
+    // CREATE
+    // add a state that will capture form inputs
+    // can update functions to each form input field which updates the respective state field
+    // create on sumbit function that takes the state object and creates a new database entry - maybe add a success response panel?
+    
+    // DELETE
+    // load in all restaurant offers to display in deletion panel
+    // add a function to the delete button which takes the offer and removes it from the database - maybe add a warning panel?
+
     const signOut = () => {
         console.log('you  signed out');
     }
 
 return (
-    <>
-    <div className="offerscontainer">
-        <div className={styles.offer}>
-            <span className={styles.offerCode}>offer id or restuarant name</span>
-            <span className={styles.offerRestaurant}>something about somthin</span>
-            <p>maybe some more information</p>
-            <button>delete</button>
-        </div> 
-    </div>
-
-
-
-
+    <section className={styles.adminPage}>
+    <h1>Admin Panel</h1>
     <div className={styles.profileBox}>
         <section className={styles.form}>
-            <h3>Add or Remove Restuarants</h3>
+            <h3>Add an Offer</h3>
             <div className={styles.form_container}>
             <form>
+                <section className = {styles.formInput}>
                 <input type="text" placeholder="Restuarant Name" required />
                 <input type="percentage" placeholder="Offer Percentage" required />
                 <input type="text" placeholder="Offer Description" required />
@@ -37,25 +36,29 @@ return (
                 <input type="text" placeholder="Restaurant Description" required />
                 <input type="text" placeholder="What type of cuisine does the restaurant serve?" required />
                 <input type="image" placeholder= "insert restuarant image here" required />
+                </section>
 
+                <section className= {styles.colorWhite}>
                 <p>What's the discount on? </p> 
-                <div class="ui checkbox"><input type="checkbox" /><label>Food</label></div>
+                <div class="ui checkbox"><label>Food</label><input type="checkbox" /></div>
                 <div class="ui checkbox"><input type="checkbox" /><label>Drink</label></div>
 
                 <p>Happy hour?</p>
                 {/* <div class="ui checkbox"><input type="checkbox"><label>True</label></div> */}
 
                 <p>Which days is the offer available?</p>
-                <div class="ui checkbox"><input type="checkbox"/><label>Monday</label></div>
+                <div class="ui checkbox" className={checkbox}><input type="checkbox"/><label>Monday</label></div>
                 <div class="ui checkbox"><input type="checkbox"/><label>Tuesday</label></div>
                 <div class="ui checkbox"><input type="checkbox"/><label>Wednesday</label></div>
                 <div class="ui checkbox"><input type="checkbox"/><label>Thursday</label></div>
                 <div class="ui checkbox"><input type="checkbox"/><label>Friday</label></div>
                 <div class="ui checkbox"><input type="checkbox"/><label>Saturday</label></div>
                 <div class="ui checkbox"><input type="checkbox"/><label>Sunday</label></div>
-                   
-                <label for="maximumTableSize">Maximum Table Size:</label>
-                <input type="range" id="a" value="12" />
+                </section>
+                {/* <label for="maximumTableSize">Maximum Table Size:</label>
+                <div class="slidecontainer"><p>Default range slider:</p>
+                <input type="range" min="1" max="100" value="50"></input></div> */}
+                
 
                 <input type="text" placeholder="Resturant Location" required />
                 
@@ -79,6 +82,33 @@ return (
             </div>
         </section>
     </div>
-    </>
+    <div className={styles.offerContainer}>
+        <h3>Delete an Offer</h3>
+        <div className={styles.offer}>
+            <span className={styles.offerCode}>offer id or restuarant name</span>
+            <span className={styles.offerRestaurant}>something about somthin</span>
+            <p>maybe some more information</p>
+            <button>Delete</button>
+        </div> 
+        <div className={styles.offer}>
+            <span className={styles.offerCode}>offer id or restuarant name</span>
+            <span className={styles.offerRestaurant}>something about somthin</span>
+            <p>maybe some more information</p>
+            <button>Delete</button>
+        </div> 
+        <div className={styles.offer}>
+            <span className={styles.offerCode}>offer id or restuarant name</span>
+            <span className={styles.offerRestaurant}>something about somthin</span>
+            <p>maybe some more information</p>
+            <button>Delete</button>
+        </div> 
+        <div className={styles.offer}>
+            <span className={styles.offerCode}>offer id or restuarant name</span>
+            <span className={styles.offerRestaurant}>something about somthin</span>
+            <p>maybe some more information</p>
+            <button>Delete</button>
+        </div> 
+    </div>
+    </section>
 )};
 export default AdminPanel;
